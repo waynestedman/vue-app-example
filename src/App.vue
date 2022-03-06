472913064eb392a4bc67c165626da698b1f95e44
Wayne Stedman <waynestedman@users.noreply.github.com>
2022-03-06 11:23:20 -0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MainApp from './components/MainApp.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <main-app msg="Hello Web Component" />
+</template>
+
+<style>
+#app {
+  /* font-family: 'Montserrat', sans-serif; */
+  font-family: 'Raleway', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MainApp.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <card-component></card-component>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
+
+<script setup>
+// import './web-components-example.es.js'
+import './card-component.js'
+
+defineProps({
+  msg: String
+})
+
+</script>
